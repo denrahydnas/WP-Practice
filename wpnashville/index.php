dd306c8d8bf32436214acd724a15a88e67eedcd5
Denrahydnas <sandyharned@Smithers.local>
2017-02-18 12:36:49 -0600

    git commit -m “initial commits for new (wpnashville) and starter child theme (2017-child)”

---
+++ wpnashville/index.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+    <?php wp_head(); ?>
+</head>
+    
+<body>
+    <h1>Annyong Haseyo</h1>
+    <p> kim su han mu kabuki wah durumi </p>  
+    <?php wp_footer(); ?>
+    
+    <?php echo get_stylesheet_directory() ?> <br>
+    <?php echo get_stylesheet_directory_uri() ?>  <br>
+    
+</body>
+
+</html>
